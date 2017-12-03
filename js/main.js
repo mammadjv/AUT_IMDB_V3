a26adaf2b83b60716094e0fc9a0995b5d6bb809d
mohammadjv6 <mohammadjv94@ymail.com>
2017-12-03 23:33:57 +0330

    main page is semi responsive

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/**
+ * Created by mohammad on 11/29/17.
+ */
+//import $ from '../js/jquery_library2.js'
+//import '../assets/owl.carousel.min.css';
+//import '../js/owl.carousel.min.js'
+
+
+$(document).ready(function() {
+    $("#owl-demo").owlCarousel({
+        rtl:true,
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        // singleItem:true,
+        autoPlay: true,
+        autoPlay: 3000,
+
+        // "singleItem:true" is a shortcut for:
+        items : 5,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+
+    });
+
+});
