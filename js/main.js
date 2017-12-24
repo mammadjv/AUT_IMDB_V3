ef1a07ea2f2c01cdf21164415a697ebc92c01cc9
mohammadjv6 <mohammadjv94@ymail.com>
2017-12-24 10:48:58 +0330

    download and like buttons added to each carousel item on image hover

--- js/main.js
+++ js/main.js
@@ -17,6 +17,29 @@ function get_id(img_id){
 }
 
 
+function clean_image_info(){
+    var img_id = get_id(this.id);
+    var title_id = "title"+img_id.toString();
+    console.log(title_id);
+    document.getElementById("item"+img_id.toString()).setAttribute('style','filter:brightness(100%);');
+    document.getElementById(title_id).setAttribute('style','opacity:0;color:white;filter:brightness(100%);');
+    document.getElementById("d_button_div"+img_id.toString()).setAttribute('style','opacity:0;');
+    document.getElementById("year"+img_id.toString()).setAttribute('style','opacity:0;');
+}
+
+
+function show_image_info(){
+    var img_id = get_id(this.id);
+    var title_id = "title"+img_id.toString();
+
+    console.log(this.id);
+
+    document.getElementById(title_id).setAttribute('style','opacity:1;color:white;filter:brightness(100%);');
+    document.getElementById("item"+img_id.toString()).setAttribute('style','filter:brightness(50%);');
+    document.getElementById("year"+img_id.toString()).setAttribute('style','opacity:1;color:yellow;margin-top: 10px;');
+    document.getElementById("d_button_div"+img_id.toString()).setAttribute('style','opacity:1;');
+}
+
 function image_clicked(){
     img_id = get_id(this.id);
     //console.log(img_id);
@@ -27,21 +50,11 @@ function image_clicked(){
     window.location=destination_url;
 }
 
-function image_mouse_hover(){
-    //var title = json_data['Title'];
-    //console.log(window.location);
-    //console.log($(this).val());
-}
-
-
-function image_mouse_out(){
-
-}
-
 
 $(document).ready(function(){
 
     var carousel_div = document.getElementById("owl-demo");
+    carousel_div.className += " row";
     for (var i = 0 ; i < film_ids.length ; i++){
         var item_div = document.createElement("div");
         item_div.className = "item";
@@ -51,18 +64,9 @@ $(document).ready(function(){
         img_div.className = "owl-carousel owl-theme";
         img_div.id = "img"+i.toString();
 
-        //var img_text = document.createElement("button");
-        //img_text.className = "centered";
-        //img_text.innerHTML = "khar";
-
-        //var ptag = document.createElement("div");
-        //ptag.id = "p"+i.toString();
-
-        //var h1 = document.createElement("h1");
-        //h1.id = "h"+i.toString();
-
-        // add events
         img_div.onclick = image_clicked;
+        img_div.onmouseover = show_image_info;
+        img_div.onmouseout = clean_image_info;
 
         item_div.appendChild(img_div);
         //item_div.appendChild(img_text);
@@ -76,30 +80,48 @@ $(document).ready(function(){
     for (var i = 0 ; i < film_ids.length ; i++) {
         var url = "http://www.omdbapi.com/?i=" + film_ids[i] + "&apikey=" + api_key;
         $.get(url).done(function (object){
-
             console.log(object);
             var element = document.getElementById("img"+received_item.toString());
+            element.className = "img_tile row";
             element.setAttribute('style','background-image:url('+object['Poster']+')'+';');
-            //element.setAttribute('src',object['Poster']);
-            //element.setAttribute('background','url(\''+object['Poster']+'\') repeat') ;
-            //var selected_item = document.getElementById("item"+received_item.toString());
-            //var p_tag = document.createElement("div");
-            //p_tag.innerHTML = "khar";
-            //p_tag.setAttribute('style','width:100px;height:100px');
 
-            //selected_item.appendChild(p_tag);
+            var film_title = document.createElement("label");
+            film_title.className = "col-xs-12";
+            film_title.id = "title"+received_item.toString();
+            film_title.innerHTML = object['Title'];
+
+            var br = document.createElement("br");
+
+            var film_year = document.createElement("label");
+            film_year.className = "col-xs-12 year";
+            film_year.id = "year"+received_item.toString();
+            film_year.innerHTML = object['Year'];
 
 
+            var download_buttons_div = document.createElement("div");
+            download_buttons_div.className= "row d_button_div btn-toolbar";
+            download_buttons_div.id= "d_button_div"+received_item.toString();
 
-            //var h1 = document.getElementById("h"+received_item.toString());
-            //ptag.innerHTML = "SALAM"
 
+            for (var  i = 0 ; i < 3 ; i++){
+                var d_button = document.createElement("button");
+                if(i == 0){
+                    d_button.className = "btn btn-default glyphicon glyphicon-download-alt col-xs-4";
+                }
+                if(i == 1){
+                    d_button.className = "btn btn-default glyphicon glyphicon-film col-xs-4";
+                }
+                if(i == 2){
+                    d_button.className = "btn btn-default glyphicon glyphicon-heart col-xs-4";
+                }
+                download_buttons_div.appendChild(d_button);
+            }
+
+            element.appendChild(film_title);
+            element.appendChild(film_year);
+            element.appendChild(download_buttons_div);
             json_array[received_item] = object;
-
-            //h1.innerHTML = "khar";
-
             element.id = "img"+received_item.toString();
-            //ptag.id = "p"+received_item.toString();
 
             received_item++;
         });
@@ -108,14 +130,35 @@ $(document).ready(function(){
     console.log(json_array.length);
     $("#owl-demo").owlCarousel({
         rtl:true,
-        navigation : true, // Show next and prev buttons
+        //navigation : true, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
         // singleItem:true,
         autoPlay: true,
         autoPlay: 3000,
+        responsiveClass:true,
+
         // "singleItem:true" is a shortcut for:
-        items : 4,
+        items : 15,
+        responsive:{
+            0:{
+                items:2,
+                nav:false
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            800:{
+                items:4,
+                nav:true,
+            },
+            1000:{
+                items:6,
+                nav:true,
+                loop:false
+            }
+        }
         // itemsDesktop : false,
         // itemsDesktopSmall : false,
         // itemsTablet: false,
